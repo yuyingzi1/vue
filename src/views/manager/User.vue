30ccd43d7eee81b17edef44786680ed2041823b2
root <123456>
2025-03-21 18:35:10 +0800

    Revert "分页"
    This reverts commit bd152377712e124037a694880289a086dcb000e2.

--- src/views/manager/User.vue
+++ src/views/manager/User.vue
@@ -14,8 +14,6 @@
       <el-table-column prop="phone" label="手机号"></el-table-column>
       <el-table-column prop="age" label="年龄"></el-table-column>
       <el-table-column prop="gender" label="性别"></el-table-column>
-      <el-table-column prop="email" label="邮箱"></el-table-column>
-      <el-table-column prop="shipping_address" label="收货地址"></el-table-column>
       <el-table-column fixed="right" label="操作">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
